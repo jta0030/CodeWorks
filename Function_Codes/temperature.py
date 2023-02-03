f5d8f7344f236a21b138d810cd6ff729edfa3313
jta0030 <68442469+jta0030@users.noreply.github.com>
2023-02-02 23:35:46 -0600

    Update and rename temperature.py to Function_Codes/temperature.py

--- Function_Codes/temperature.py
+++ Function_Codes/temperature.py
@@ -1,6 +1,6 @@
 #this code shows the temperature of the board reads
 
-import time
+
 from adafruit_circuitplayground import cp
 
 
@@ -11,14 +11,9 @@ def fahrenheit(celsius):                #this is a function to convert C to F
 
 while True:
     temp = fahrenheit(cp.temperature)   #reads the temperature of board and converts temp to F
-    if temp > 80:                       #if temp above 80 F turn led on
-        cp.red_led = True
-    else:
-        cp.red_led = False              #if temp not above 80 F turn led off
     print((temp,))                      #prints and plots the temperature in plotter
 
-    time.sleep(.2)
-
+    
 #temperature might not be 100% accurate but can be used to see drastic changes in temperature
 #try testing with a cold and hot environment (DO NOT submerge)
 
